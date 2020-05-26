a4926c81bc696b3e508557c818470b9240cfd2ae
Leonardo Losoviz <leo@getpop.org>
2020-05-26 19:13:41 +0800

    Skip schema when initializing component

--- src/Component.php
+++ src/Component.php
@@ -33,10 +33,11 @@ class Component extends AbstractComponent
     /**
      * Initialize services
      */
-    protected static function doInitialize(): void
+    protected static function doInitialize(bool $skipSchema = false): void
     {
-        parent::doInitialize();
+        parent::doInitialize($skipSchema);
         self::initYAMLServices(dirname(__DIR__));
+        self::maybeInitYAMLSchemaServices(dirname(__DIR__), $skipSchema);
     }
 
     /**
